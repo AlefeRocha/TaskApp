4eb9c76a2af85e05230d12c7963d9ae36dcff407
AlefeRocha <alefe.rtavares@gmail.com>
2023-01-27 16:33:52 -0300

    adding and changing the forms and containers, to receive images.

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -30,11 +30,34 @@ class _FormScreenState extends State<FormScreen> {
           child: Column(
             children: [
               Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Container(
+                  height: 100,
+                  width: 72,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(width: 2, color: Colors.blue),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      imageController.text,
+                      errorBuilder: (BuildContext context, Object exception,
+                          StackTrace? stackTrace) {
+                        return Image.asset('assets/images/nophoto.png');
+                      },
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: TextFormField(
                   controller: nameController,
                   textAlign: TextAlign.start,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Nome',
                     fillColor: Colors.white70,
@@ -47,7 +70,7 @@ class _FormScreenState extends State<FormScreen> {
                 child: TextFormField(
                   controller: difficultyController,
                   textAlign: TextAlign.start,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Dificuldade',
                     fillColor: Colors.white70,
@@ -58,9 +81,14 @@ class _FormScreenState extends State<FormScreen> {
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: TextFormField(
+                  onChanged: (text){
+                    setState(() {
+
+                    });
+                  },
                   controller: imageController,
                   textAlign: TextAlign.start,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     border: OutlineInputBorder(),
                     hintText: 'Imagem',
                     fillColor: Colors.white70,
@@ -71,10 +99,10 @@ class _FormScreenState extends State<FormScreen> {
               ElevatedButton(
                 onPressed: () {
                   print(nameController.text);
-                  print(difficultyController.text);
+                  print(int.parse(difficultyController.text));
                   print(imageController.text);
                 },
-                child: Text('Adicionar'),
+                child: const Text('Adicionar'),
               ),
             ],
           ),
