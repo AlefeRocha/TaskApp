9c39a6f4a037915e6d6d4b7a06b5717e72763903
AlefeRocha <alefe.rtavares@gmail.com>
2023-02-02 21:54:42 -0300

    Editing inherited file.

--- lib/data/task_inherited.dart
+++ lib/data/task_inherited.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:tarefas/components/tasks.dart';
 
 class TaskInherited extends InheritedWidget {
-  const TaskInherited({
+  TaskInherited({
     Key? key,
     required Widget child,
   }) : super(key: key, child: child);
 
-  final List taskList = [];
+  final List<Task> taskList = [Task('Aprender Flutter', 'assets/images/flutter_img.png', 3)];
+
+  void newTask(String name, String photo, int difficulty){
+    taskList.add(Task(name, photo, difficulty));
+  }
 
   static TaskInherited of(BuildContext context) {
     final TaskInherited? result = context.dependOnInheritedWidgetOfExactType<TaskInherited>();
