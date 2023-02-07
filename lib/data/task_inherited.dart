9afb176bcfab1c820fc3b29cad53eb0f73e4b965
AlefeRocha <alefe.rtavares@gmail.com>
2023-02-06 21:57:35 -0300

    Editing inherited file.

--- lib/data/task_inherited.dart
+++ lib/data/task_inherited.dart
@@ -7,20 +7,28 @@ class TaskInherited extends InheritedWidget {
     required Widget child,
   }) : super(key: key, child: child);
 
-  final List<Task> taskList = [Task('Aprender Flutter', 'assets/images/flutter_img.png', 3)];
+  final List<Task> taskList = [
+    Task('Estudar Flutter', 'assets/images/flutter_img.png', 4),
+    Task(
+        'Lavar o carro para o passeio', 'assets/images/lavar_carro_img.jpg', 1),
+    Task('Compras', 'assets/images/compras_img.jpg', 2),
+    Task('Trabalho', 'assets/images/trabalho_img.webp', 5),
+    Task('Academia', 'assets/images/academia_img.jpeg', 3),
+  ];
 
-  void newTask(String name, String photo, int difficulty){
+  void newTask(String name, String photo, int difficulty) {
     taskList.add(Task(name, photo, difficulty));
   }
 
   static TaskInherited of(BuildContext context) {
-    final TaskInherited? result = context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+    final TaskInherited? result =
+        context.dependOnInheritedWidgetOfExactType<TaskInherited>();
     assert(result != null, 'No TaskInherited found in context');
     return result!;
   }
 
   @override
-  bool updateShouldNotify(TaskInherited old) {
-    return ;
+  bool updateShouldNotify(TaskInherited oldWidget) {
+    return oldWidget.taskList.length != taskList.length;
   }
 }
--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tarefas/components/tasks.dart';
+// import 'package:tarefas/components/tasks.dart';
+import 'package:tarefas/data/task_inherited.dart';
 import 'package:tarefas/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
@@ -17,17 +18,7 @@ class _InitialScreenState extends State<InitialScreen> {
         title: const Text('Tarefas'),
       ),
       body: ListView(
-        children: const [
-          Task('Estudar Flutter', 'assets/images/flutter_img.png', 4),
-          Task('Lavar o carro para o passeio',
-              'assets/images/lavar_carro_img.jpg', 1),
-          Task('Compras', 'assets/images/compras_img.jpg', 2),
-          Task('Trabalho', 'assets/images/trabalho_img.webp', 5),
-          Task('Academia', 'assets/images/academia_img.jpeg', 3),
-          SizedBox(
-            height: 80,
-          )
-        ],
+        children: TaskInherited.of(context).taskList,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
